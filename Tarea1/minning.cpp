6e0ea275d03eebb1b0126ef80a4dda6fc406207d
JoseBarrera <chapixjoestar04@gmail.com>
2024-11-11 01:52:11 -0500

    Se inicia la Uva Minning y se crea la carpeta para almacenar los test

--- Tarea1/minning.cpp
+++ Tarea1/minning.cpp
@@ -3,3 +3,14 @@
  * Ingenieria en Sistemas
  * Minning - Tarea 1
  */
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+ int S, W, C, K, M; // TiempoLlegarUbicacion, ExcavaDurante, UnidadesRecurso, numeroRobots, minutosFabricarRobot
+ int cantRobots, tiempoTotal, recursosRecolectados;
+ cin >> S >> W >> C >> K >> M;
+ return 0;
+}
